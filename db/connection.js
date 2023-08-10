a76afe41bd183f2c96cd2e994ebb04b3c5f968fa
Avinash Kumar <63252165+avi-3012@users.noreply.github.com>
2023-08-10 18:08:43 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 const DB = process.env.MONGO_URL;
-//"mongodb+srv://ecommerce:xGJEgASN8vpYnSC5@ecommerce.u5g95rd.mongodb.net/?retryWrites=true&w=majority";
+
 
 mongoose.connect(DB);
